df6542496b406afed601e139703e87ce93844c41
Michael Currie <mcurrie@bruceforceresearch.com>
2015-02-22 23:42:31 -0700

    Skeleton #TODOs

--- pptl2qb.py
+++ pptl2qb.py
@@ -13,36 +13,55 @@ http://dev.ticketleap.com/
 
 """
 
-#import petl as etl
-
-#tl = etl.fromtsv('ticketleap.csv')
-#pp = etl.fromtsv('paypal.csv')
+# TODO: import petl as etl
 
 import sys
+import datetime
 
 def usage():
     print("pptl2qb.py --start=20150101 --end=20150228.  Both --start and --end are optional")
 
 def main(argv):
-    for arg in argv:
-        print(arg)
-    
-    # TODO: command-line arguments    
-    # http://www.diveintopython.net/scripts_and_streams/command_line_arguments.html
-    generate_output()
-
-
-def generate_output():
+    #for arg in argv:
+    #    print(arg)
+    input_pp = 'paypal.csv'
+    input_tl = 'ticketleap.csv'
+    start_date = datetime.date(2015, 1, 1)
+    end_date = None
     output_iif = 'output.iif'
     output_unprocessed = 'unprocessed.csv'
     
-    output_iif_file = get_output_file(output_iif)
-    output_unprocessed_file = get_output_file(output_unprocessed)
+    # TODO: command-line arguments    
+    # http://www.diveintopython.net/scripts_and_streams/command_line_arguments.html
+
+    # TODO: open input_pp, input_tl
+    #tl = etl.fromtsv('ticketleap.csv')
+    #pp = etl.fromtsv('paypal.csv')
+
+    # TODO: transform those rows as necessary
+    # follow this use case:
+    # http://nbviewer.ipython.org/github/alimanfoo/petl/blob/master/notes/case_study_1.ipynb
     
-    output_iif_file.write('test succeeded')
+    generate_output(output_iif)
+
+    output_unprocessed_file = get_output_file(output_unprocessed)
+    # TODO: add all the unprocessed rows to output_unprocessed_file
+    output_unprocessed_file.close()
+
+
+def generate_output(output_iif):
+    
+    output_iif_file = get_output_file(output_iif)
+    
+    output_iif_file.write('!TRNS	DATE	ACCNT	NAME	CLASS	AMOUNT	MEMO\n')
+    output_iif_file.write('!SPL	DATE	ACCNT	NAME	AMOUNT	MEMO\n')
+    output_iif_file.write('!ENDTRNS\n')
+    
+    # TODO:
+    # add all of the transactions    
+    # maybe use a dict for what's in the transaction?
     
     output_iif_file.close()
-    output_unprocessed_file.close()
     
 
 def get_output_file(file_name):
