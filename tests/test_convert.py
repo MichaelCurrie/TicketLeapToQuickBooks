31078866eb95d16597613a4ba4de2116073f817e
Michael Currie <mcurrie@bruceforceresearch.com>
2015-05-06 16:32:57 -0600

    Fixing utf-8 encoding error
    Apparently paypal encodes its strings in Latin1, instead of utf-8.  So
    I'll have to do an in-place conversion.

--- tests/test_convert.py
+++ tests/test_convert.py
@@ -6,6 +6,7 @@ Created on Wed May  6 12:09:46 2015
 """
 
 import sys, os, datetime
+import codecs
 
 # We must add .. to the path so that we can perform the 
 # import of the package while running this as 
@@ -20,12 +21,22 @@ def test_conversion():
     OUTPUT: output.iif and unprocessed.csv
     
     """
+    #paypal_path = "C:\\Users\\mcurrie\\Desktop\\GitHub\\TicketLeapToQuickBooks\\tests\\paypal_example.csv"
     paypal_path = os.path.join(os.path.normpath(os.path.dirname(__file__)),
                                'paypal_example.csv')
 
-    pp2qb.paypal_to_quickbooks(paypal_path, 
-                               start_date=datetime.date(2015, 1, 1))
+    try:
+        f = codecs.open(paypal_path, encoding='utf-8', errors='strict')
+        for line in f:
+            pass
+        print("Valid utf-8")
+        pp2qb.paypal_to_quickbooks(paypal_path, 
+                                   start_date=datetime.date(2015, 1, 1))
+    except UnicodeDecodeError:
+        print("invalid utf-8")
 
+        # TODO: Convert the csv file in-place to utf-8.
+        # http://stackoverflow.com/questions/6539881/python-converting-from-iso-8859-1-latin1-to-utf-8
 
 
 if __name__ == "__main__":
